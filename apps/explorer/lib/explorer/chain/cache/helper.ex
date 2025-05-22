b0f5692897da4de3795200b61bc08e0895ca49dd
Leoni Mella <leoni.mella@gmail.com>
2025-05-22 14:42:58 -0300

    feat: update estimate count stats query

--- apps/explorer/lib/explorer/chain/cache/helper.ex
+++ apps/explorer/lib/explorer/chain/cache/helper.ex
@@ -11,9 +11,18 @@ defmodule Explorer.Chain.Cache.Helper do
   @doc """
     Estimates the row count of a given table using PostgreSQL system catalogs.
 
+    Old method:
     This function executes a query to estimate the number of rows in the specified
     table based on the table's reltuples and relpages values from the pg_class catalog.
-    It provides a fast estimation rather than an exact count.
+    It provides a fast estimation rather than an exact count:
+    ```
+    "SELECT (CASE WHEN c.reltuples < 0 THEN NULL WHEN c.relpages = 0 THEN float8 '0' ELSE c.reltuples / c.relpages END * (pg_catalog.pg_relation_size(c.oid) / pg_catalog.current_setting('block_size')::int))::bigint FROM pg_catalog.pg_class c WHERE c.oid = '#{table_name}'::regclass"
+    ```
+
+    Modification for Somnia:
+    Previous query was providing a very off number when querying Somnia database due to the
+    high transactions throughput. We updated to the new query bellow since it is a more
+    accurate number, but still an approximation.
 
     ## Parameters
     - `table_name`: The name of the table to estimate the row count for.
@@ -26,9 +35,7 @@ defmodule Explorer.Chain.Cache.Helper do
   @spec estimated_count_from(binary()) :: non_neg_integer() | nil
   def estimated_count_from(table_name, options \\ []) do
     %Postgrex.Result{rows: [[count]]} =
-      Chain.select_repo(options).query!(
-        "SELECT (CASE WHEN c.reltuples < 0 THEN NULL WHEN c.relpages = 0 THEN float8 '0' ELSE c.reltuples / c.relpages END * (pg_catalog.pg_relation_size(c.oid) / pg_catalog.current_setting('block_size')::int))::bigint FROM pg_catalog.pg_class c WHERE c.oid = '#{table_name}'::regclass"
-      )
+      Chain.select_repo(options).query!("SELECT n_live_tup FROM pg_stat_all_tables WHERE relname = '#{table_name}';")
 
     count
   end
