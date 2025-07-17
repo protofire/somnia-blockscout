6fa51e8893cbb7fb86f7f454db9beb788e34e36b
Leoni Mella <leoni.mella@gmail.com>
2025-07-17 14:01:29 -0300

    fix: remove foreign key from table

--- apps/explorer/priv/repo/migrations/20241031110127_create_pending_transaction_operations.exs
+++ apps/explorer/priv/repo/migrations/20241031110127_create_pending_transaction_operations.exs
@@ -3,11 +3,7 @@ defmodule Explorer.Repo.Migrations.CreatePendingTransactionOperations do
 
   def change do
     create table(:pending_transaction_operations, primary_key: false) do
-      add(:transaction_hash, references(:transactions, column: :hash, on_delete: :delete_all, type: :bytea),
-        null: false,
-        primary_key: true
-      )
-
+      add(:transaction_hash, :bytea, null: false, primary_key: true)
       timestamps()
     end
   end
